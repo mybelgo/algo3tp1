dff32c7b92dfa6e417f96f1e3fbfa4305f98fd80
Tomas Miguez <tomasmiguez99@gmail.com>
2021-04-25 19:03:00 -0300

    Cree un programita para generar casos de prueba.
    Se le pasa por parametro la distribucion, hasta ahora solo u.
    Y por std hay que ingresarle:
    "pathDeOutput"
    "minVal" "maxVal"
    "numeroDeCasosParaCadaN" "numeroDeNsDistintosAGenerar"
    "primerN" "segundoN" ...

---
+++ testCases/generador/generator.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <chrono>
+#include <random>
+#include <fstream>
+
+using namespace std;
+
+map<string, string> distribuciones_implementadas = {
+    {"u", "Uniforme"}
+};
+
+int main(int argc, char** argv){
+    if (argc < 2 || distribuciones_implementadas.find(argv[1]) == distribuciones_implementadas.end())
+	{
+		cerr << "Distribucion no encontrada: " << argv[1] << endl;
+		cerr << "Las distribuciones existentes son: " << endl;
+		for (auto& alg_desc: distribuciones_implementadas) cerr << "\t- " << alg_desc.first << ": " << alg_desc.second << endl;
+		return 0;
+	}
+    string distribucion = argv[1];
+
+    int steps, n;
+    int minVal, maxVal;
+    string path;
+    cin >> path;
+    cin >> minVal >> maxVal;
+    cin >> n >> steps;
+    vector<int> points(steps, 0);
+    for(int i=0; i<steps; i++) cin >> points[i];
+
+    std::random_device rd;
+    std::mt19937::result_type seed = rd() ^ (
+            (std::mt19937::result_type)
+            std::chrono::duration_cast<std::chrono::seconds>(
+                std::chrono::system_clock::now().time_since_epoch()
+                ).count() +
+            (std::mt19937::result_type)
+            std::chrono::duration_cast<std::chrono::microseconds>(
+                std::chrono::high_resolution_clock::now().time_since_epoch()
+                ).count() );
+
+    std::mt19937 gen(seed);
+    std::uniform_int_distribution<int> distrib(minVal, maxVal);
+
+    for(int i=0; i<steps; i++) {
+        for(int j=0; j<n; j++) {
+            string filename = "n" + to_string(points[i]) + "_" + to_string(j) + ".in";
+            ofstream out(path + "/" + filename);
+
+            out << points[i] << " " << distrib(gen);
+            for(int k=0; k<points[i]; k++) out << "\n" << distrib(gen) << " " << distrib(gen);
+
+            out.close();
+        }
+    }
+}
