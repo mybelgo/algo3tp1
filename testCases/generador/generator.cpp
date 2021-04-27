28297861badd07e0a83438cbd6f757ab4f039e95
Tomas Miguez <tomasmiguez99@gmail.com>
2021-04-27 19:57:25 -0300

    No me juzguen por este commit xfa.

--- testCases/generador/generator.cpp
+++ testCases/generador/generator.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 map<string, string> distribuciones_implementadas = {
-    {"u", "Uniforme"}
+    {"uniforme", "Uniforme"}, {"uniformeR", "Uniforme R"}, {"muchos", "Muchos productos"}, {"pocos", "Pocos productos"}, {"fullRandom", "Full Random"}
 };
 
 int main(int argc, char** argv){
@@ -25,6 +25,7 @@ int main(int argc, char** argv){
     int r_tubo, minVal, maxVal;
     string path;
     cin >> path;
+    path = path + "/" + distribucion;
     cin >> r_tubo >> minVal >> maxVal;
     cin >> n >> steps;
     vector<int> points(steps, 0);
@@ -42,20 +43,85 @@ int main(int argc, char** argv){
                 ).count() );
 
     std::mt19937 gen(seed);
-    std::uniform_int_distribution<int> distrib(minVal, maxVal);
+    
 
     ofstream indice(path + "/indice.csv");
     indice << "dataset,instancia,n,W,archivo";
 
     for(int i=0; i<steps; i++) {
         for(int j=0; j<n; j++) {
-            string filename = "n" + to_string(points[i]) + "_" + to_string(j) + ".in";
-            ofstream out(path + "/" + filename);
+            ofstream out;
 
-            out << points[i] << " " << r_tubo << "\n";
-            for(int k=0; k<points[i]; k++) out << distrib(gen) << " " << distrib(gen) << "\n";
+            if(distribucion == "uniforme") {
+                std::uniform_int_distribution<int> distrib(minVal, maxVal);
 
-            indice << "\n" << distribucion + "," + to_string(j) + "," + to_string(points[i]) + "," + to_string(r_tubo) + "," + path + "/" + filename;
+                int nActual = points[i];
+                r_tubo = distrib(gen);
+
+                string filename = "n" + to_string(nActual) + "_" + to_string(j) + ".in";
+                out.open(path + "/" + filename);
+
+                out << nActual << " " << r_tubo << "\n";
+                for(int k=0; k<nActual; k++) out << distrib(gen) << " " << distrib(gen) << "\n";
+
+                indice << "\n" << distribucion + "," + to_string(j) + "," + to_string(nActual) + "," + to_string(r_tubo) + "," + path + "/" + filename;
+            }
+            else if(distribucion == "muchos") {
+                std::uniform_int_distribution<int> distrib(minVal, 2*r_tubo/points[i]);
+
+                int nActual = points[i];
+
+                string filename = "n" + to_string(nActual) + "_" + to_string(j) + ".in";
+                out.open(path + "/" + filename);   
+
+
+                out << nActual << " " << r_tubo << "\n";
+                for(int k=0; k<nActual; k++) out << distrib(gen) << " " << r_tubo << "\n";
+
+                indice << "\n" << distribucion + "," + to_string(j) + "," + to_string(nActual) + "," + to_string(r_tubo) + "," + path + "/" + filename;
+            }
+            else if(distribucion == "pocos") {
+                std::uniform_int_distribution<int> distrib(0.2*r_tubo, 1.1*r_tubo);
+                
+                int nActual = points[i];
+
+                string filename = "n" + to_string(nActual) + "_" + to_string(j) + ".in";
+                out.open(path + "/" + filename);
+
+                out << nActual << " " << r_tubo << "\n";
+                for(int k=0; k<nActual; k++) out << distrib(gen) << " " << r_tubo << "\n";
+
+                indice << "\n" << distribucion + "," + to_string(j) + "," + to_string(nActual) + "," + to_string(r_tubo) + "," + path + "/" + filename;
+            }
+            else if(distribucion == "uniformeR") {
+                std::uniform_int_distribution<int> distrib(minVal, maxVal);
+
+                int nActual = distrib(gen);
+                r_tubo = points[i];
+
+                string filename = "r" + to_string(r_tubo) + "_" + to_string(j) + ".in";
+                out.open(path + "/" + filename);
+
+                out << nActual << " " << r_tubo << "\n";
+                for(int k=0; k<nActual; k++) out << distrib(gen) << " " << distrib(gen) << "\n";
+
+                indice << "\n" << distribucion + "," + to_string(j) + "," + to_string(nActual) + "," + to_string(r_tubo) + "," + path + "/" + filename;
+            }
+            else if(distribucion == "fullRandom") {
+                std::uniform_int_distribution<int> distrib(minVal, maxVal);
+
+                int nActual = distrib(gen);
+                r_tubo = distrib(gen);
+
+                string filename = to_string(i*n+j) + ".in";
+                out.open(path + "/" + filename);
+
+                out << nActual << " " << r_tubo << "\n";
+                for(int k=0; k<nActual; k++) out << distrib(gen) << " " << distrib(gen) << "\n";
+
+                indice << "\n" << distribucion + "," + to_string(i*n+j) + "," + to_string(nActual) + "," + to_string(r_tubo) + "," + path + "/" + filename;
+            }
+
             out.close();
         }
     }
